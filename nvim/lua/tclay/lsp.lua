9c0645c389e1b99977b36502aae0470a9e1ba861
tsclay <tsclay9@gmail.com>
2024-01-07 21:12:45 -0500

    comment lua_ls config in favor of neodev

--- nvim/lua/tclay/lsp.lua
+++ nvim/lua/tclay/lsp.lua
@@ -16,6 +16,18 @@
 --     end,
 --   })
 -- end
+local function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
 
 local on_attach = function(_, bufnr)
   local nmap = function(keys, func, desc)
@@ -107,49 +119,55 @@ local servers = {
   html = { filetypes = { 'html', 'twig', 'hbs' } },
   cssls = {},
 
-  lua_ls = {
-    on_init = function(client)
-      local path = client.workspace_folders[1].name
-      if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-        client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-          Lua = {
-            diagnostics = {
-              globals = { 'vim' },
-            },
-            runtime = {
-              -- Tell the language server which version of Lua you're using
-              -- (most likely LuaJIT in the case of Neovim)
-              version = 'LuaJIT',
-            },
-            -- Make the server aware of Neovim runtime files
-            workspace = {
-              checkThirdParty = false,
-              library = {
-                vim.env.VIMRUNTIME,
-                -- "${3rd}/luv/library"
-                -- "${3rd}/busted/library",
-              },
-              -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-              -- library = vim.api.nvim_get_runtime_file("", true)
-              telemetry = { enable = false },
-            },
-          },
-        })
-        client.notify('workspace/didChangeConfiguration', { settings = client.config.settings })
-      end
-      return true
-    end,
-    -- Lua = {
-    --   diagnostics = {
-    --     globals = { 'vim' },
-    --   },
-    --   workspace = {
-    --     checkThirdParty = false,
-    --     library = vim.api.nvim_get_runtime_file('', true),
-    --   },
-    --   telemetry = { enable = false },
-    -- },
-  },
+  -- lua_ls = {
+  --   on_init = function(client)
+  --     local path = client.workspace_folders[1].name
+  --     if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+  --       print("shoud do things")
+  --       client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+  --         Lua = {
+  --           diagnostics = {
+  --             globals = { 'vim' },
+  --           },
+  --           runtime = {
+  --             -- Tell the language server which version of Lua you're using
+  --             -- (most likely LuaJIT in the case of Neovim)
+  --             version = 'LuaJIT',
+  --           },
+  --           -- Make the server aware of Neovim runtime files
+  --           workspace = {
+  --             checkThirdParty = false,
+  --             library = {
+  --               vim.env.VIMRUNTIME,
+  --               -- "${3rd}/luv/library"
+  --               -- "${3rd}/busted/library",
+  --             },
+  --             -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+  --             -- library = vim.api.nvim_get_runtime_file("", true)
+  --           },
+  --         },
+  --       })
+  --       print(dump(client.config.settings))
+  --       client.notify('workspace/didChangeConfiguration', { settings = client.config.settings })
+  --     end
+  --     return true
+  --   end,
+  --   settings = {
+  --     Lua = {
+  --       telemetry = false
+  --     }
+  --   }
+  --   -- Lua = {
+  --   --   diagnostics = {
+  --   --     globals = { 'vim' },
+  --   --   },
+  --   --   workspace = {
+  --   --     checkThirdParty = false,
+  --   --     library = vim.api.nvim_get_runtime_file('', true),
+  --   --   },
+  --   --   telemetry = { enable = false },
+  --   -- },
+  -- },
 }
 -- local attach_overrides = {
 --   lua_ls = {
