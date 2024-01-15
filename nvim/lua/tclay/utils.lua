fab3d0920fbc3e980c1285684ac99c00636817aa
tsclay <tsclay9@gmail.com>
2024-01-14 22:19:31 -0500

    Refactor venv display in lualine

--- nvim/lua/tclay/utils.lua
+++ nvim/lua/tclay/utils.lua
@@ -22,10 +22,10 @@ function M.dirname(path)
   if not path or #path == 0 then
     return
   end
-  print('the path before ', path)
+  -- print('the path before ', path)
   local result = path:gsub(strip_sep_pat, ''):gsub(strip_dir_pat, '')
   -- print(path:gsub(strip_sep_pat, ''))
-  print('the result ', result)
+  -- print('the result ', result)
   if #result == 0 then
     if vim.fn.has 'win32' then
       return path:sub(1, 2):upper()
@@ -52,7 +52,7 @@ function M.get_python_path()
   -- Find and use virtualenv in workspace directory.
   for _, pattern in ipairs { '*', '.*' } do
     local match = vim.fn.glob(path.join(workspace:gsub('[/\\]$', ''), pattern, 'pyvenv.cfg'))
-    print(match)
+    -- print(match)
     if match ~= '' then
       if vim.fn.has 'win32' == 1 then
         return M.dirname(match) .. 'Scripts' .. '/python.exe'
