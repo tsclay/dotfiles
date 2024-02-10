32d4e7268e3af36d6d72883efaefc623ebd0d550
tsclay <tsclay9@gmail.com>
2024-02-10 13:11:03 -0500

    Use opaque bkgd for float windows

--- nvim/after/plugin/transparent.lua
+++ nvim/after/plugin/transparent.lua
@@ -22,13 +22,27 @@ require('transparent').setup {
     'SignColumn',
     'CursorLineNr',
     'EndOfBuffer',
+    -- below are from default config
+    'CursorLine',
+    'StatusLine',
+    'StatusLineNC',
   },
   extra_groups = {
-    'NormalFloat', -- plugins which have float panel such as Lazy, Mason, LspInfo
-    'NvimTreeNormal', -- NvimTree
+    -- 'NormalFloat', -- plugins which have float panel such as Lazy, Mason, LspInfo
+    -- 'NvimTreeNormal', -- NvimTree
   },
-  exclude_groups = {
-  }, -- table: groups you don't want to clear
+  exclude_groups = {}, -- table: groups you don't want to clear
 }
 
 vim.api.nvim_command 'set shortmess=I'
+
+-- local state = "false"
+-- local cache = assert(io.open(vim.fn.stdpath('data') .. '/transparent_cache'), 'r')
+-- if cache ~= nil then
+--   state = cache:read '*a'
+--   cache:close()
+-- end
+-- print('state is ', state)
+-- if state == "true" then
+--   vim.cmd[[:TransparentEnable]]
+-- end
