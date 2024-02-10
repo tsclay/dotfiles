4b509ff74fa73329825d6376f33b9021f3564fb6
tsclay <tsclay9@gmail.com>
2024-02-10 13:08:32 -0500

    Add oil.nvim; ensure cd is set to dir that is opened

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,16 @@
---
+local cdpwd = vim.api.nvim_create_augroup('cdpwd', { clear = true})
+vim.api.nvim_create_autocmd('VimEnter', {
+  group = cdpwd,
+  callback = function (data)
+    local dir = string.match(data.file, "^[a-z]+://(.+)$")
+    vim.api.nvim_set_current_dir(dir)
+  end
+})
+
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
+vim.opt.title = true
+vim.opt.titlestring = [[%t – %{fnamemodify(getcwd(), ':t')}]]
 -- vim.g.db_ui_use_nerd_fonts = 1
 -- vim.g.db_ui_dotenv_variable_prefix = 'DATABASE_URL_'
 -- vim.g.db_ui_execute_on_save = 0
