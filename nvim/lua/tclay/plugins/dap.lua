6139e05783f03a6df5ade3949f13d0023ac7ed51
tsclay <tsclay9@gmail.com>
2024-02-16 22:56:37 -0500

    Fix args for vim.fn.input

--- nvim/lua/tclay/plugins/dap.lua
+++ nvim/lua/tclay/plugins/dap.lua
@@ -131,7 +131,7 @@ return {
             return '127.0.0.1'
           end,
           port = function()
-            local val = tonumber(vim.fn.input('Port: ', '54321'))
+            local val = tonumber(vim.fn.input { prompt = 'Port: ', default = '54321' })
             assert(val, 'Please provide a port number')
             return val
           end,
