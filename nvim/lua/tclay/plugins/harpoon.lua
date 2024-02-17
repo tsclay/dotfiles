cf1b099b904c5d6ddc2b8e32c3d67a6792d5be74
tsclay <tsclay9@gmail.com>
2024-02-17 09:28:47 -0500

    Change harpoon keybinds

--- nvim/lua/tclay/plugins/harpoon.lua
+++ nvim/lua/tclay/plugins/harpoon.lua
@@ -12,23 +12,27 @@ return {
 
       vim.keymap.set('n', '<leader>a', function()
         harpoon:list():append()
-      end)
+      end, { desc = 'Add current file to Harpoon' })
+
       vim.keymap.set('n', '<C-e>', function()
         harpoon.ui:toggle_quick_menu(harpoon:list())
-      end)
+      end, { desc = 'Open Harpoon menu' })
 
-      vim.keymap.set('n', '<C-h>', function()
+      vim.keymap.set('n', '<leader>1', function()
         harpoon:list():select(1)
-      end)
-      vim.keymap.set('n', '<C-t>', function()
+      end, { desc = 'Open Harpoon file 1' })
+
+      vim.keymap.set('n', '<leader>2', function()
         harpoon:list():select(2)
-      end)
-      vim.keymap.set('n', '<C-n>', function()
+      end, { desc = 'Open Harpoon file 2' })
+
+      vim.keymap.set('n', '<leader>3', function()
         harpoon:list():select(3)
-      end)
-      vim.keymap.set('n', '<C-s>', function()
+      end, { desc = 'Open Harpoon file 3' })
+
+      vim.keymap.set('n', '<leader>4', function()
         harpoon:list():select(4)
-      end)
+      end, { desc = 'Open Harpoon file 4' })
     end,
   },
 }
