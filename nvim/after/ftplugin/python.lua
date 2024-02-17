27b0e2d5e678c4d008ea7d6f0fdb4454596a26fc
tsclay <tsclay9@gmail.com>
2024-02-16 22:38:25 -0500

    Setup mypy lint config only after opening a python file

---
+++ nvim/after/ftplugin/python.lua
@@ -0,0 +1,35 @@
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+  callback = function()
+    if vim.bo.filetype == 'python' then
+      require('lint').try_lint { 'mypy' }
+    end
+  end,
+})
+
+local lint = require('lint')
+local py_exe = vim.lsp.get_active_clients({name = 'pyright'})[1].config.settings.python.pythonPath
+
+local mypy = lint.linters.mypy
+mypy.args = {
+  '--python-executable',
+  py_exe,
+  '--show-column-numbers',
+  '--hide-error-context',
+  '--no-color-output',
+  '--no-error-summary',
+  '--no-pretty',
+  '--ignore-missing-imports',
+  '--disallow-untyped-defs',
+}
+local groups = { 'file', 'lnum', 'col', 'severity', 'message', 'code' }
+local severities = {
+  error = vim.diagnostic.severity.ERROR,
+  warning = vim.diagnostic.severity.WARN,
+  note = vim.diagnostic.severity.HINT,
+}
+local pat = '([^:]+):(%d+):(%d+): (%a+): (.*)  %[([%a-]+)'
+mypy.parser = require('lint.parser').from_pattern(pat, groups, severities, { ['source'] = 'mypy' }, { end_col_offset = 0 })
+
+lint.linters_by_ft = {
+  python = { 'mypy' },
+}
