5ee5ad3003833f0de44b10bb2cc15e6c51846a71
tsclay <tsclay9@gmail.com>
2024-02-19 00:07:08 -0500

    Fix syntax hl in md files for noctis themes

--- nvim/lua/tclay/plugins/colors.lua
+++ nvim/lua/tclay/plugins/colors.lua
@@ -5,12 +5,23 @@ return {
     priority = 0,
   },
   {
-    'talha-akram/noctis.nvim',
-    -- init = function()
-    --   vim.cmd 'colorscheme noctis_azureus'
-    --   -- needed for transparent.nvim so that colorscheme is correctly set on toggles
-    --   vim.g.colors_name = 'noctis_azureus'
-    -- end,
+    'tsclay/noctis.nvim',
+    init = function()
+      require('noctis_theme').setup({
+        NormalBold = { fg = 'blue', bg = 'none', bold = true },
+        NormalItalic = { fg = 'blue', bg = 'none', italic = true },
+      }, {
+        ['@markup.underline'] = { link = 'Underlined' },
+        ['@markup.italic'] = { link = 'NormalItalic' },
+        ['@markup.strong'] = { link = 'NormalBold' },
+        ['@markup.heading'] = { link = 'Title' },
+        ['@markup.raw'] = { link = 'Comment' },
+        ['@markup.link'] = { link = 'Identifier' },
+        ['@markup.link.url'] = { link = 'helpURL' },
+        ['@markup.link.label'] = { link = 'Identifier' },
+        ['@markup.list'] = { link = 'Todo' },
+      })
+    end,
   },
   {
     'lalitmee/cobalt2.nvim',
@@ -20,6 +31,7 @@ return {
     -- require('colorbuddy').colorscheme 'cobalt2'
     -- end,
   },
+  { 'catppuccin/nvim', name = 'catppuccin' },
 }
 -- if vim.api.nvim_command ':colorscheme' == nil or vim.api.nvim_command ':colorscheme' == 'default' then
 --   vim.api.nvim_command ':colorscheme noctis_azureus'
