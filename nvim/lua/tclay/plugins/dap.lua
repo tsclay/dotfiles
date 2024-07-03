7ae6699dcaead2b32780aa61c002877d96f54d5e
tsclay <tsclay9@gmail.com>
2024-07-02 21:59:13 -0400

    chore: update nvim-dap

--- nvim/lua/tclay/plugins/dap.lua
+++ nvim/lua/tclay/plugins/dap.lua
@@ -6,6 +6,7 @@ return {
     dependencies = {
       -- Creates a beautiful debugger UI
       'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
 
       -- Installs the debug adapters for you
       'williamboman/mason.nvim',
--- nvim/lua/tclay/plugins/dapui.lua
+++ nvim/lua/tclay/plugins/dapui.lua
@@ -1,6 +1,7 @@
 return {
   {
     'rcarriga/nvim-dap-ui',
+    dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' },
     config = function()
       local dap = require 'dap'
       local dapui = require 'dapui'
@@ -13,17 +14,78 @@ return {
         --    Don't feel like these are good choices.
         icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
         controls = {
+          element = 'repl',
+          enabled = true,
           icons = {
-            pause = '⏸',
-            play = '▶',
-            step_into = '⏎',
-            step_over = '⏭',
-            step_out = '⏮',
-            step_back = 'b',
-            run_last = '▶▶',
-            terminate = '⏹',
-            disconnect = '⏏',
+            disconnect = '',
+            pause = '',
+            play = '',
+            run_last = '',
+            step_back = '',
+            step_into = '',
+            step_out = '',
+            step_over = '',
+            terminate = '',
           },
+        },
+        element_mappings = {},
+        expand_lines = true,
+        floating = {
+          border = 'single',
+          mappings = {
+            close = { 'q', '<Esc>' },
+          },
+        },
+        force_buffers = true,
+        layouts = {
+          {
+            elements = {
+              {
+                id = 'scopes',
+                size = 0.25,
+              },
+              {
+                id = 'breakpoints',
+                size = 0.25,
+              },
+              {
+                id = 'stacks',
+                size = 0.25,
+              },
+              {
+                id = 'watches',
+                size = 0.25,
+              },
+            },
+            position = 'left',
+            size = 40,
+          },
+          {
+            elements = {
+              {
+                id = 'repl',
+                size = 0.5,
+              },
+              {
+                id = 'console',
+                size = 0.5,
+              },
+            },
+            position = 'bottom',
+            size = 10,
+          },
+        },
+        mappings = {
+          edit = 'e',
+          expand = { '<CR>', '<2-LeftMouse>' },
+          open = 'o',
+          remove = 'd',
+          repl = 'r',
+          toggle = 't',
+        },
+        render = {
+          indent = 1,
+          max_value_lines = 100,
         },
       }
 
