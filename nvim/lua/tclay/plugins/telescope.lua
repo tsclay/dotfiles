d08b288601663ad39f7974434593d0e600346e6f
tsclay <tsclay9@gmail.com>
2024-07-02 22:00:20 -0400

    feat: add some QoL extensions to telescope

--- nvim/lua/tclay/plugins/telescope.lua
+++ nvim/lua/tclay/plugins/telescope.lua
@@ -2,10 +2,19 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+        'nvim-telescope/telescope-live-grep-args.nvim',
+        --This will not install any breaking changes.
+        --For major updates, this must be adjusted manually.
+        version = '^1.0.0',
+      },
+    },
     config = function()
       -- [[ Configure Telescope ]]
       -- See `:help telescope` and `:help telescope.setup()`
+      local lga_actions = require 'telescope-live-grep-args.actions'
       require('telescope').setup {
         defaults = {
           mappings = {
@@ -16,14 +25,35 @@ return {
           },
           layout_strategy = 'vertical',
         },
+        extensions = {
+          live_grep_args = {
+            auto_quoting = true, --enable/disable auto-quoting
+            -- define mappings, e.g.
+            mappings = { -- extend mappings
+              i = {
+                ['<C-k>'] = lga_actions.quote_prompt(),
+                ['<C-i>'] = lga_actions.quote_prompt { postfix = ' --iglob' },
+                ['<C-t>'] = lga_actions.quote_prompt { postfix = ' -t' },
+                ['<C-h>'] = lga_actions.quote_prompt { postfix = ' --hidden' },
+                ['<C-s>'] = lga_actions.quote_prompt { postfix = ' --hidden --no-ignore --iglob' },
+              },
+            },
+            -- ...also accepts theme settings, for example:
+            -- theme = "dropdown", -- use dropdown theme
+            -- theme = { }, -- use own theme spec
+            -- layout_config = { mirror=true }, -- mirror preview pane
+          },
+        },
       }
 
       -- Enable telescope fzf native, if installed
       pcall(require('telescope').load_extension, 'fzf')
+      pcall(require('telescope').load_extension, 'live_grep_args')
 
       -- See `:help telescope.builtin`
       vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
       vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[h] Search the Help' })
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to telescope to change theme, layout, etc.
         require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
@@ -51,6 +81,7 @@ return {
       end, { desc = 'Search project for text under cursor up to next whitespace' })
 
       vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>fsg', require('telescope').extensions.live_grep_args.live_grep_args, { desc = '[F]ilter [S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
     end,
   },
