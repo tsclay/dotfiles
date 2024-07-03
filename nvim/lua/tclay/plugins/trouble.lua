4d0e76afacec55fbfb8a4e4bada50b70392eefe1
tsclay <tsclay9@gmail.com>
2024-07-02 21:59:55 -0400

    chore: update trouble

--- nvim/lua/tclay/plugins/trouble.lua
+++ nvim/lua/tclay/plugins/trouble.lua
@@ -5,26 +5,38 @@ return {
     opts = {
       auto_preview = false,
     },
-    config = function()
-      vim.keymap.set(
-        'n',
-        '<leader>qw',
-        '<cmd>TroubleToggle workspace_diagnostics<cr>',
-        { silent = true, noremap = true, desc = 'Show diagnostics for workspace' }
-      )
-      vim.keymap.set(
-        'n',
-        '<leader>qd',
-        '<cmd>TroubleToggle document_diagnostics<cr>',
-        { silent = true, noremap = true, desc = 'Show diagnostics for current file' }
-      )
-      vim.keymap.set('n', '<leader>qq', '<cmd>TroubleToggle quickfix<cr>', { silent = true, noremap = true, desc = 'Show quickfix list' })
-      -- vim.keymap.set('n','<leader>ql', '<cmd>TroubleToggle loclist<cr>', { silent = true, noremap = true, desc = 'Show location list' })
-      -- vim.keymap.set('n','gR', '<cmd>TroubleToggle lsp_references<cr>', { silent = true, noremap = true, desc = 'Show LSP references in Trouble' })
-      -- vim.keymap.set("n", "<leader>qw", function() require("trouble").toggle("workspace_diagnostics") end)
-      -- vim.keymap.set("n", "<leader>qf", function() require("trouble").toggle("document_diagnostics") end)
-      -- vim.keymap.set('n','<leader>q', '<cmd>TroubleToggle<cr>', { silent = true, noremap = true, desc = 'Show diagnostics' })
-    end,
+    keys = {
+      {
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
+    },
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
